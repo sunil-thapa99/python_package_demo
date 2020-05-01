2ea83a1cdd30cc34ebeac73af8fbf364e8e35846
lappanchappan43 <lappanchappan43@gmail.com>
2020-05-01 12:35:11 +0545

    Python package demo

---
+++ gaussian/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
+
